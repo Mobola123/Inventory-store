095d894dcabdd88d86dd752df2e5bed0b7ae0983
Support <Support@WEMA-TRAINING-9>
2023-11-24 08:54:11 +0100

    Add project files.

---
+++ Model/BakeryProduct.cs
@@ -0,0 +1,47 @@
+﻿using confectionerystore;
+using System;
+namespace Inventory_store.Model
+{
+
+    enum BakingCategory
+    {
+        Bread, Cake, Pastry, Cookie
+    }
+
+    record BakeryProduct(string ProductName, decimal Price, BakingCategory Category);
+
+    interface IBakeryActions
+    {
+        public void AddProduct(BakeryProduct product);                  // Method to add a product to the store
+        public void DisplayProducts();                                  // Method showing information about products in the store
+    }
+
+    class BakingStore : IBakeryActions
+    {
+        List<BakeryProduct> products;
+
+        public string StoreName { get; set; }
+
+
+        public BakingStore(string storeName)                  // Constructor    
+        {
+            StoreName = storeName;
+            products = new List<BakeryProduct>();
+        }
+        public void AddProduct(BakeryProduct product)        // Method to add a product to the store
+        {
+            products.Add(product);
+        }
+
+        public void DisplayProducts()
+        {
+            Console.WriteLine($"Products in {StoreName}:");
+
+            foreach (var product in products)
+            {
+                Console.WriteLine($"{product.ProductName}, Category: {product.Category}, Price: N{product.Price}");
+            }
+        }
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿namespace confectionerystore;
+using Inventory_store.Model;
+class Program
+{
+    public static void Main(string[] args)
+    {
+        
+        BakingStore myBakery = new BakingStore("Mobola Confectioneries");
+        Console.WriteLine($"Welcome to {myBakery.StoreName}!");
+
+        myBakery.AddProduct(new BakeryProduct("Sweetened coconut bread", 3.50m, BakingCategory.Bread));
+        myBakery.AddProduct(new BakeryProduct("Chocolate Cake", 25.99m, BakingCategory.Cake));
+        myBakery.AddProduct(new BakeryProduct("Meatpie", 4.95m, BakingCategory.Pastry));
+        myBakery.AddProduct(new BakeryProduct("Double chocoCookies", 8.99m, BakingCategory.Cookie));
+
+        myBakery.DisplayProducts();
+    }
+}
